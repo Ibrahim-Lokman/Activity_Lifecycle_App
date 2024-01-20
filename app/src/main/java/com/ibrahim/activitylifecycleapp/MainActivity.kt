d015eefbc27e0a049a5ad8eeee276c6395a68d78
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2024-01-20 20:37:07 +0600

    onStart, onResume, onPause, onStart

--- app/src/main/java/com/ibrahim/activitylifecycleapp/MainActivity.kt
+++ app/src/main/java/com/ibrahim/activitylifecycleapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ibrahim.activitylifecycleapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +13,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "I'm in onCreate()", Toast.LENGTH_SHORT).show()
         Log.d("Sajeet Create", "I'm in onCreate()")
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "I'm in onStart()", Toast.LENGTH_SHORT).show()
+        Log.d("Sajeet Start", "I'm in onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "I'm in onResume()", Toast.LENGTH_SHORT).show()
+        Log.d("Sajeet Resume", "I'm in onResume()")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "I'm in onPause()", Toast.LENGTH_SHORT).show()
+        Log.d("Sajeet Pause", "I'm in onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "I'm in onStop()", Toast.LENGTH_SHORT).show()
+        Log.d("Sajeet Stop", "I'm in onStop()")
     }
 
     override fun onDestroy() {
